33f9477c1b5617ebfe173fcba7778aeca0f3c045
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-08-17 20:20:55 +0500

    Setup entity configuration classes

---
+++ Nauman.AIPortfolioGenerator/Persistence/Configurations/SectionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nauman.AIPortfolioGenerator.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class SectionConfiguration : IEntityTypeConfiguration<Section>
+    {
+        public void Configure(EntityTypeBuilder<Section> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Nauman.AIPortfolioGenerator/Persistence/Configurations/TemplateConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nauman.AIPortfolioGenerator.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class TemplateConfiguration : IEntityTypeConfiguration<Template>
+    {
+        public void Configure(EntityTypeBuilder<Template> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
