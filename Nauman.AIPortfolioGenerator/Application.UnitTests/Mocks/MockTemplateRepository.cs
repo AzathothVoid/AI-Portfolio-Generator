57d35feffdae997a10e97ed20c673c9140407570
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-08-20 16:46:08 +0500

    setting up testing framework

---
+++ Nauman.AIPortfolioGenerator/Application.UnitTests/Mocks/MockTemplateRepository.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using Nauman.AIPortfolioGenerator.Domain;
+using Nauman.AIPortfolioGenerator.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Application.UnitTests.Mocks
+{
+    public static class MockTemplateRepository
+    {
+        public static Mock<ITemplateRepository> GetTemplateRepository()
+        {
+            var templates = new List<Nauman.AIPortfolioGenerator.Domain.Template>
+            {
+                new Nauman.AIPortfolioGenerator.Domain.Template
+                {
+                    Id = 1,
+                    Name = "Web Developer CV",
+                    Description = "A fine CV for a sauve and professional look",
+                    Keywords = new string[] { "Professional", "Portfolio", "Modern" },
+                    Themes = new string[] { "Professional", "Upbright" },
+                    Palette = new string[] { "#000", "#111" },
+                    IsPaid = false
+                },
+                new Nauman.AIPortfolioGenerator.Domain.Template
+                {
+                    Id = 2,
+                    Name = "AI Developer CV",
+                    Description = "A fine CV for a automated engineer like youself",
+                    Keywords = new string[] { "Professional", "Portfolio", "Modern" },
+                    Themes = new string[] { "Professional", "Upbright" },
+                    Palette = new string[] { "#000111", "#111000" },
+                    IsPaid = true
+                }
+            };
+
+            var mockRepo = new Mock<ITemplateRepository>();
+
+            mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(templates);
+            mockRepo.Setup(r => r.AddAsync(It.IsAny<Nauman.AIPortfolioGenerator.Domain.Template>())).ReturnsAsync((Nauman.AIPortfolioGenerator.Domain.Template template) =>
+            {
+                templates.Add(template);
+                return template;
+            });
+
+            return mockRepo;
+        }
+    }
+}
---
+++ Nauman.AIPortfolioGenerator/Application.UnitTests/Template/Queries/GetTemplateListRequestHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using Application.UnitTests.Mocks;
+using AutoMapper;
+using Moq;
+using Nauman.AIPortfolioGenerator.Application.Contracts.Persistence;
+using Nauman.AIPortfolioGenerator.Application.DTOs.Template;
+using Nauman.AIPortfolioGenerator.Application.Features.Templates.Handlers.Queries;
+using Nauman.AIPortfolioGenerator.Application.Features.Templates.Requests.Queries;
+using Nauman.AIPortfolioGenerator.Application.Mapper;
+using Nauman.AIPortfolioGenerator.Application.Responses;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests.Template.Queries
+{
+    public class GetTemplateListRequestHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ITemplateRepository> _mockRepo;
+        public GetTemplateListRequestHandlerTests()
+        {
+            _mockRepo = MockTemplateRepository.GetTemplateRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<AutoMappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetTemplateListTest()
+        {
+            var handler = new GetTemplateListRequestHandler(_mockRepo.Object, _mapper);
+
+            var result = await handler.Handle(new GetTemplateListRequest(), CancellationToken.None);
+
+            result.ShouldBeOfType<CustomQueryResponse<List<TemplateDTO>>>();
+            result.Success.ShouldBe(true);
+            result.Errors.ShouldBeNull();
+            result.Data.ShouldBeOfType<List<TemplateDTO>>();
+            result.Data.Count.ShouldBe(2);
+        }
+    }
+}
