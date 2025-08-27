9b70fe6365fa4919c3593ce422faa17c235e23c7
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-08-27 21:19:00 +0500

    Authentication service creation

---
+++ Nauman.AIPortfolioGenerator/Client/Contracts/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Client.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Authenticate(string email, string password);
+        Task<bool> Register();
+        Task Logout();
+    }
+}
---
+++ Nauman.AIPortfolioGenerator/Client/Services/AuthenticationService.cs
@@ -0,0 +1,30 @@
+﻿using Client.Contracts;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Client.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public Task<bool> Authenticate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<bool> Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IList<Claim> ParseClaims(JwtSecurityToken tokenContent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
