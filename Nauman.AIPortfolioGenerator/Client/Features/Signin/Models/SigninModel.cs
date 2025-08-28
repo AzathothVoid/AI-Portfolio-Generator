3efdfdc690a2398a015f010cd92811992995c3b8
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-08-28 23:22:34 +0500

    signup page initial complete

---
+++ Nauman.AIPortfolioGenerator/Client/Features/Signin/Models/SigninModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Features.Signin.Models
+{
+    public class SignInModel
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool AgreeToTerms { get; set; }
+    }
+}
