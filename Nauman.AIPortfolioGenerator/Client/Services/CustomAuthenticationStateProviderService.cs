b3c843459f980c6ddcf5c9cdbbf5e64dc1c0bf3a
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-08-30 23:36:23 +0500

    adding client side authorization pipelie

---
+++ Nauman.AIPortfolioGenerator/Client/Services/CustomAuthenticationStateProviderService.cs
@@ -0,0 +1,28 @@
+﻿using Client.Contracts;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Client.Services
+{
+    public class CustomAuthenticationStateProviderService : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public CustomAuthenticationStateProviderService(ILocalStorageService  localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyUserAuthentication(List<Claim> claims)
+        {
+            var identity = new ClaimsIdentity(claims, "jwt");
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
