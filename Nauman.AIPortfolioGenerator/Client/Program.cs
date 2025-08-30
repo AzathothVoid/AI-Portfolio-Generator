ae84cbbb533a89483084db5d43d683d43de4b69b
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-08-30 23:57:20 +0500

    bug fix

--- Nauman.AIPortfolioGenerator/Client/Program.cs
+++ Nauman.AIPortfolioGenerator/Client/Program.cs
@@ -2,13 +2,14 @@ using Client;
 using Client.Contracts;
 using Client.Services;
 using Client.Services.Base;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSingleton(ILocalStorageService, LocalStorageService);
+builder.Services.AddSingleton<ILocalStorageService, LocalStorageService>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProviderService>();
 builder.Services.AddAuthorizationCore();
 
