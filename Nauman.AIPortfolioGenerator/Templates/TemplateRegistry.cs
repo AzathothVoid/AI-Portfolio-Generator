cce664a7ab8d45e7ba5fcba65fb8b950263514c2
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-09-10 22:46:21 +0500

    Template project references complete

--- Nauman.AIPortfolioGenerator/Templates/TemplateRegistry.cs
+++ Nauman.AIPortfolioGenerator/Templates/TemplateRegistry.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Templates.Template1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace Templates
 {
-    public class TemplateRegistry
+    public record TemplateInfo(string Id, string Name, string PreviewUrl, Type RootComponent);
+    public static class TemplateRegistry
     {
+        public static IReadOnlyList<TemplateInfo> All { get; } = new[]
+        {
+          
+            new TemplateInfo(
+                Id: "modern-one",
+                Name: "Modern One",
+                PreviewUrl: "/_content/Nauman.AIPortfolioGenerator.Templates/preview/modern-one.png",
+                RootComponent: typeof(Templates.Template1.Root) 
+            ),           
+        };
     }
 }
